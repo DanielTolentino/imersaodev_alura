e9a33bca1e03da2bb723ad6bbefa7e55f5798306
Daniel Tolentino <danieltolentino56@gmail.com>
2021-03-22 22:00:02 -0300

    Update script.js
    Atualiza a cotação do dólar para o dia 22/03/2021 no fechamento do dia.

--- conversor-de-moedas/src/script.js
+++ conversor-de-moedas/src/script.js
@@ -2,7 +2,7 @@ function converter(){
 var moeda = document.getElementById('entrada').value
 parseFloat(moeda)
 
-var valorEmReal = moeda * 5.53
+var valorEmReal = moeda * 5.51
 var valorEmRealDecimal = valorEmReal.toFixed(2)
 var valoremRealVir = valorEmRealDecimal.replace('.', ',')
 
