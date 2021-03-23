d53ab0d716f6122eb33fb3f08af0d8f8708cfd22
Daniel Tolentino <danieltolentino56@gmail.com>
2021-03-23 11:09:32 -0300

    Reduz números de variáveis
    Simplifica a lógica e número de variáveis para otimizar o código.
    O cálculo passa ser executado apenas na linha da variável 'valorEmReal'.

--- conversor-de-moedas/dist/script.js
+++ conversor-de-moedas/dist/script.js
@@ -2,10 +2,10 @@ function converter(){
 var moeda = document.getElementById('entrada').value
 parseFloat(moeda)
 
-var valorEmReal = moeda * 5.51
-var valorEmRealDecimal = valorEmReal.toFixed(2)
-var valoremRealVir = valorEmRealDecimal.replace('.', ',')
+var valorEmReal = (moeda * 5.51).toFixed(2).replace('.' , ',')
+//var valorEmRealDecimal = valorEmReal.toFixed(2)
+//var valoremRealVir = valorEmRealDecimal.replace('.', ',')
 
 var valorEmReal =  
-document.getElementById('resultado').value= "R$" + valoremRealVir;
+document.getElementById('resultado').value= "R$" + valorEmReal;
 }
