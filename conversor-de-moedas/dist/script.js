2aecf314db6f34bffbcf946280ba90d3d7abae99
Daniel Tolentino <danieltolentino56@gmail.com>
2021-04-02 02:10:48 -0300

    Introduz API de cotação em tempo real
    Puxa da API os valores atualizados a cada 30 segundos.

--- conversor-de-moedas/dist/script.js
+++ conversor-de-moedas/dist/script.js
@@ -1,16 +1,48 @@
+//API para consultar valor do dólar via fetch
+const URL_TO_FETCH = 'https://economia.awesomeapi.com.br/json/all/USD-BRL,EUR-BRL,BTC-BRL';
+fetch(URL_TO_FETCH, {
+  method: 'get' // opcional
+})
+.then(function(response) {
+  
+    response.json()
+
+    .then(function(result) {
+     dolarHoje = result.USD["ask"]
+     eurHoje = result.EUR["ask"]
+     btcHoje = result.BTC["ask"]
+        /* var json = JSON.parse(dolarHoje)
+        console.log(json) */
+    })
+})
+.catch(function(err) { 
+  console.error(err);
+});
+
 //Cria a função para multiplicar o valor em real por dólar, e mostar na tela o resultado
 function converter(){
-    //recebe o input digitado pelo usuário no HTML
+       //recebe o input digitado pelo usuário no HTML
 var moeda = document.getElementById('entrada').value
 //converte o valor digitado para um número Float, com cadas decimais
 parseFloat(moeda)
 
 //multiplica o valor da variável 'moeda', deixa 2 casas decimais fixas, e substitui o ponto por vírgulas
-var valorEmReal = (moeda * 5.71).toFixed(2).replace('.' , ',')
-//var valorEmRealDecimal = valorEmReal.toFixed(2)
-//var valoremRealVir = valorEmRealDecimal.replace('.', ',')
+var valorEmReal = (moeda * dolarHoje)
+var valorEmEuro = (moeda * eurHoje)
+var valorEmBtc = (moeda * btcHoje)
 
-//passa para o elemento 'resultado' no HTML e exibe o valor para o usuário
 var valorEmReal =  
-document.getElementById('resultado').value= "R$" + valorEmReal;
+document.getElementById('resultado-dolar').value= "R$" + valorEmReal.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+var valorEmEuro =  
+document.getElementById('resultado-euro').value= "R$" + valorEmEuro.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+var valorEmBtc =  
+document.getElementById('resultado-btc').value= "R$" + valorEmBtc.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+
+    //exibe a cotação após a conversão
+    var divCotacaoHoje = document.getElementById("valor-atual")
+    var htmlDolar = "Dólar:" + valorEmReal
+    var htmlEuro = " Euro:" + valorEmEuro
+    var htmlBitcoin = " BTC:" + valorEmBtc
+  
+    divCotacaoHoje.innerHTML = htmlDolar + htmlEuro + htmlBitcoin 
 }
